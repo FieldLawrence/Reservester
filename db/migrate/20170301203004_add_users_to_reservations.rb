c6ba8c516a4e3cde52fb7d7a400edc1c09a7970b
Lawrence Field <lfield@bu.edu>
2017-03-07 14:07:23 -0500

    added foreign keys

--- db/migrate/20170301203004_add_users_to_reservations.rb
+++ db/migrate/20170301203004_add_users_to_reservations.rb
@@ -1,4 +1,5 @@
 class AddUsersToReservations < ActiveRecord::Migration[5.0]
   def change
+    add_reference :reservations, :user, index: true, foreign_key: true
   end
 end
--- db/migrate/20170301203018_add_restaurants_to_reservations.rb
+++ db/migrate/20170301203018_add_restaurants_to_reservations.rb
@@ -1,4 +1,5 @@
 class AddRestaurantsToReservations < ActiveRecord::Migration[5.0]
   def change
+    add_reference :restaurants, :reservation, index: true, foreign_key: true
   end
 end
